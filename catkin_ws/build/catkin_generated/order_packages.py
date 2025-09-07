0a1591baa2d84be201320e1ca3d3f64b076991bf
SmartOmaru <omar2004j@gmail.com>
2025-09-08 02:29:43 +0300

    Add catkin_ws

---
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/ares/git/mia-task-12-1/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/ares/git/mia-task-12-1/catkin_ws/devel;/home/ares/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/ares/git/mia-task-12-1/catkin_ws/devel;/home/ares/catkin_ws/devel;/opt/ros/noetic' != '' else []
